69acbec9c04e44a8b275004e6b1832a7e416dc5a
louiis <louis.chihara@gmail.com>
2016-02-06 10:19:43 -0600

    Update cachematrix.R
    Added '...' to the solve arguments.

--- cachematrix.R
+++ cachematrix.R
@@ -28,7 +28,7 @@ cacheSolve <- function(x, ...) {
                 return(inv)
         }
         data <- x$get()
-        inv <- solve(data)
+        inv <- solve(data,...)
         x$setinverse(inv)
         return(inv)
 }
